46072ac17bdac209d3d69606874c33011b00106a
ramit-kundu <132339637+ramit-kundu@users.noreply.github.com>
2023-06-11 18:18:55 +0530

    add error framework

---
+++ backend/serror/serror.go
@@ -0,0 +1,31 @@
+package serror
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+// Serror represents a structured error.
+type Serror interface {
+	GetError() error //gets error
+}
+
+// serror is the implementation of the Serror interface.
+type serror struct {
+	err   error
+	stack []byte
+}
+
+// GetError returns the underlying error.
+func (s *serror) GetError() error {
+	return s.err
+}
+
+// NewSerror creates a new instance of Serror.
+func NewSerror(err error) Serror {
+	fmt.Printf("Error: %v\nStack Trace:\n%s\n", err, debug.Stack()) //log error with stacktrace during conversion
+	return &serror{
+		err:   err,
+		stack: debug.Stack(),
+	}
+}
